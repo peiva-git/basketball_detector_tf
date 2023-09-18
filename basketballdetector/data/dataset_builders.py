a4369c534a0d290509b42c0c92e148e5f511d4a2
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-18 14:32:10 +0200

    Moved sequences to different module

--- basketballdetector/data/dataset_builders.py
+++ basketballdetector/data/dataset_builders.py
@@ -1,17 +1,12 @@
 import glob
-import math
 import pathlib
 import os.path
 from typing import Any
-from random import shuffle
 
 import tensorflow as tf
 import numpy as np
 
-
-def decode_image(image_data, image_width: int = 50, image_height: int = 50, channels: int = 3):
-    image = tf.io.decode_png(image_data, channels=channels)
-    return tf.image.resize(image, [image_height, image_width])
+from basketballdetector.data import decode_image
 
 
 def configure_for_performance(dataset: tf.data.Dataset, buffer_size: int, batch_size: int) -> tf.data.Dataset:
@@ -75,69 +70,6 @@ class ClassificationDatasetBuilder:
     def configure_datasets_for_performance(self, shuffle_buffer_size: int = 10000, input_batch_size: int = 32):
         self.__train_dataset = configure_for_performance(self.__train_dataset, shuffle_buffer_size, input_batch_size)
         self.__validation_dataset = self.__validation_dataset.batch(input_batch_size)
-
-
-class ClassificationSequenceBuilder:
-    def __init__(self, data_directory: str, batch_size: int, validation_percentage: float = 0.2):
-        data_path = pathlib.Path(data_directory)
-        print('Gathering all image paths...')
-        image_paths = [
-            image_path
-            for image_path in glob.iglob(str(data_path / '*/*/*/*.png'))
-        ]
-        shuffle(image_paths)
-        print(f'Found {len(image_paths)} images')
-        validation_size = int(len(image_paths) * validation_percentage)
-        validation_paths = image_paths[:validation_size]
-        training_paths = image_paths[validation_size:]
-        print(f'{len(validation_paths)} images in validation dataset')
-        print(f'{len(training_paths)} images in training dataset')
-        self.__training_sequence = ClassificationSequence(data_directory, training_paths, batch_size)
-        self.__validation_sequence = ClassificationSequence(data_directory, validation_paths, batch_size)
-
-    @property
-    def training_sequence(self):
-        return self.__training_sequence
-
-    @property
-    def validation_sequence(self):
-        return self.__validation_sequence
-
-
-class ClassificationSequence(tf.keras.utils.Sequence):
-    def __init__(self, data_directory: str, images_paths: list[str], batch_size: int):
-        data_path = pathlib.Path(data_directory)
-        self.__batch_size = batch_size
-        self.__image_paths = images_paths
-        self.__class_names = np.unique(sorted([item.name for item in data_path.glob('*/*/*')]))
-        print(f'Found classes {self.__class_names}')
-
-    def __getitem__(self, index):
-        low = index * self.__batch_size
-        high = min(low + self.__batch_size, len(self.__image_paths))
-        batch_paths = self.__image_paths[low:high]
-        batch_labels = [
-            self.__get_label(tf.constant(image_path))
-            for image_path in batch_paths
-        ]
-        batch_images = [
-            self.__get_image(tf.constant(image_path))
-            for image_path in batch_paths
-        ]
-        return tf.stack(batch_images), tf.stack(batch_labels)
-
-    def __len__(self):
-        return math.ceil(len(self.__image_paths) / self.__batch_size)
-
-    def __get_label(self, file_path: tf.Tensor) -> int:
-        parts = tf.strings.split(file_path, os.path.sep)
-        one_hot = parts[-2] == self.__class_names
-        return tf.one_hot(tf.argmax(one_hot), 2)
-
-    @staticmethod
-    def __get_image(file_path: tf.Tensor):
-        image_data = tf.io.read_file(file_path)
-        return decode_image(image_data, image_width=112, image_height=112, channels=3)
 
 
 class SegmentationDatasetBuilder:
---
+++ basketballdetector/data/sequence_builders.py
@@ -0,0 +1,74 @@
+import pathlib
+import glob
+import math
+import os
+
+from random import shuffle
+
+import tensorflow as tf
+import numpy as np
+
+from basketballdetector.data import decode_image
+
+
+class ClassificationSequenceBuilder:
+    def __init__(self, data_directory: str, batch_size: int, validation_percentage: float = 0.2):
+        data_path = pathlib.Path(data_directory)
+        print('Gathering all image paths...')
+        image_paths = [
+            image_path
+            for image_path in glob.iglob(str(data_path / '*/*/*/*.png'))
+        ]
+        shuffle(image_paths)
+        print(f'Found {len(image_paths)} images')
+        validation_size = int(len(image_paths) * validation_percentage)
+        validation_paths = image_paths[:validation_size]
+        training_paths = image_paths[validation_size:]
+        print(f'{len(validation_paths)} images in validation dataset')
+        print(f'{len(training_paths)} images in training dataset')
+        self.__training_sequence = ClassificationSequence(data_directory, training_paths, batch_size)
+        self.__validation_sequence = ClassificationSequence(data_directory, validation_paths, batch_size)
+
+    @property
+    def training_sequence(self):
+        return self.__training_sequence
+
+    @property
+    def validation_sequence(self):
+        return self.__validation_sequence
+
+
+class ClassificationSequence(tf.keras.utils.Sequence):
+    def __init__(self, data_directory: str, images_paths: list[str], batch_size: int):
+        data_path = pathlib.Path(data_directory)
+        self.__batch_size = batch_size
+        self.__image_paths = images_paths
+        self.__class_names = np.unique(sorted([item.name for item in data_path.glob('*/*/*')]))
+        print(f'Found classes {self.__class_names}')
+
+    def __getitem__(self, index):
+        low = index * self.__batch_size
+        high = min(low + self.__batch_size, len(self.__image_paths))
+        batch_paths = self.__image_paths[low:high]
+        batch_labels = [
+            self.__get_label(tf.constant(image_path))
+            for image_path in batch_paths
+        ]
+        batch_images = [
+            self.__get_image(tf.constant(image_path))
+            for image_path in batch_paths
+        ]
+        return tf.stack(batch_images), tf.stack(batch_labels)
+
+    def __len__(self):
+        return math.ceil(len(self.__image_paths) / self.__batch_size)
+
+    def __get_label(self, file_path: tf.Tensor) -> int:
+        parts = tf.strings.split(file_path, os.path.sep)
+        one_hot = parts[-2] == self.__class_names
+        return tf.one_hot(tf.argmax(one_hot), 2)
+
+    @staticmethod
+    def __get_image(file_path: tf.Tensor):
+        image_data = tf.io.read_file(file_path)
+        return decode_image(image_data, image_width=112, image_height=112, channels=3)
