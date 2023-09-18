9ef236c9896422d6863f48408269467f56bd94ea
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-18 18:26:19 +0200

    Exporting tasks

---
+++ basketballdetector/tasks/__init__.py
@@ -0,0 +1,3 @@
+from .predicting import write_image_sequence_from_video, write_detections_video
+from .training.train_mobilenet_classifier import train_and_save_model as train_mobilenet
+from .training.train_efficientnet_classifier import train_and_save_model as train_efficientnet
