52cd1cf128432d717141ec686bc57b9e16168a90
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-19 16:25:08 +0200

    Displaying README on front page

--- basketballdetector/__init__.py
+++ basketballdetector/__init__.py
@@ -1,6 +1,6 @@
-from .tasks.training import train_efficientnet_classifier, train_mobilenet_classifier
-from .tasks.predicting import write_image_sequence_from_video
-
 """
 .. include:: ../README.md
 """
+
+from .tasks.training import train_efficientnet_classifier, train_mobilenet_classifier
+from .tasks.predicting import write_image_sequence_from_video
