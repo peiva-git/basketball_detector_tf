9f4c3a0b94065d800d80d73b129493487c0a742e
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-20 11:49:23 +0200

    Correction to docs

--- basketballdetector/data/__init__.py
+++ basketballdetector/data/__init__.py
@@ -4,7 +4,7 @@ The `basketballdetector.data.dataset_builders.ClassificationDatasetBuilder` and 
 `basketballdetector.data.dataset_builders.SegmentationDatasetBuilder` classes
 both load the entire dataset into memory before training,
 while the `basketballdetector.data.sequence_builders.ClassificationSequenceBuilder` and the
-`basketballdetector.data.sequence_builers.PatchesSequence` rely on the Keras Sequence class.
+`basketballdetector.data.sequence_builders.PatchesSequence` rely on the Keras Sequence class.
 More information can be found here https://www.tensorflow.org/api_docs/python/tf/keras/utils/Sequence
 """
 
